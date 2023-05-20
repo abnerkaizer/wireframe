d8514c8204928301d6a2ac6be9cb24c471425305
Abner Kaizer <abnerkaizer@protonmail.com>
2023-05-20 11:17:09 -0300

    destaques completo

--- src/pages/Popular/index.jsx
+++ src/pages/Popular/index.jsx
@@ -6,48 +6,119 @@ const Popular = () => {
   const movieURL = import.meta.env.VITE_API;
   const apiKey = import.meta.env.VITE_API_KEY;
   const genres = [
-    "Action",
-    "Adventure",
-    "Animation",
-    "Comedy",
-    "Documentary",
-    "Drama",
-    "Family",
-    "Fantasy",
-    "History",
-    "Horror",
-    "Music",
-    "Mystery",
-    "Romance",
-    "Science Fiction",
-    "TV Movie",
-    "Thriller",
-    "War",
-    "Western",
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentary",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Family",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 36,
+      name: "History",
+    },
+    {
+      id: 27,
+      name: "Horror",
+    },
+    {
+      id: 10402,
+      name: "Music",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 10770,
+      name: "TV Movie",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
   ];
+
   const [popularMovies, setPopularMovies] = useState([]);
+  const [originalPopularMovies, setOriginalPopularMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const getPopularMovies = async (url) => {
     const resp = await fetch(url);
     const data = await resp.json();
     setPopularMovies(data.results);
+    setOriginalPopularMovies(data.results);
+    setIsLoading(false);
   };
+
   useEffect(() => {
     const popularUrl = `${movieURL}popular?${apiKey}`;
     getPopularMovies(popularUrl);
   }, []);
-  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedOption, setSelectedOption] = useState(Number);
 
   const handleChange = (event) => {
     event.preventDefault();
-    const value = event.target.value;
+    const value = parseInt(event.target.value);
     setSelectedOption(value);
   };
   useEffect(() => {
     console.log(selectedOption);
-    setPopularMovies(
-      popularMovies.filter((movie) => selectedOption === movie.genre)
-    );
+    if (selectedOption) {
+      const filteredMovies = originalPopularMovies.filter((movie) =>
+        movie.genre_ids.includes(selectedOption)
+      );
+      setPopularMovies(filteredMovies);
+    } else {
+      setPopularMovies(originalPopularMovies);
+    }
   }, [selectedOption]);
+
   return (
     <div className="container">
       <div className="top">
@@ -56,16 +127,20 @@ const Popular = () => {
         <h2 className="title">Destaques: </h2>
         <select defaultValue="Selecione uma opção" onChange={handleChange}>
           <option disabled>Selecione uma opção</option>
-          {genres.map((genre, index) => (
-            <option key={index} value={genre}>
-              {genre}
+          {genres.map((genre) => (
+            <option key={genre.id} value={genre.id}>
+              {genre.name}
             </option>
           ))}
         </select>
       </div>
       <div className="movies-container">
-        {popularMovies.length === 0 && <p>Carregando...</p>}
-        {popularMovies.length > 0 &&
+        {isLoading && <p>Carregando...</p>}
+        {!isLoading && popularMovies.length === 0 && (
+          <p>Não há filmes desse gênero.</p>
+        )}
+        {!isLoading &&
+          popularMovies.length > 0 &&
           popularMovies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
