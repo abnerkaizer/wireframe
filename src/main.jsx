cb8de5d53d1df5d4c4b32c9ce5f9ec17d61a5cc9
Abner Kaizer <abnerkaizer@protonmail.com>
2023-05-29 20:35:02 -0300

    home /wireframe

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/wireframe" element={<Home />} />
           <Route path="movie/:id" element={<Movie />} />
           <Route path="/search" element={<Search />} />
           <Route path="/upcoming" element={<Upcoming />} />
